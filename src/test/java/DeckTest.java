4d5a94e33f732c0160578ca420cdb58aa4fc6917
Luca Sanz Charreun <lucasanz2471@gmail.com>
2020-02-13 13:58:06 +0000

    deck starts empty

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    private Deck deck;
+
+    @Before
+    public void before() {
+        deck = new Deck();
+    }
+
+    @Test
+    public void cardsStartEmpty() {
+        assertEquals(0, deck.getNumberOfCards());
+    }
+
+
 }
